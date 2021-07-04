606c46738964e09e8c01de4f26431b32319b42f3
EvgeniyKostin <kostinen@icloud.com>
2021-07-04 12:04:54 +0300

    Initial Commit

---
+++ SOLIDPrinciplesUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLIDPrinciplesUIKit
+//
+//  Created by Evgeniy Kostin on 04.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
